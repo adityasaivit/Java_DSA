dd9f1a8555710712df901be7b0b70e67137fa8b0
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-07-20 10:34:40 +0530

    Add files via upload

--- charecter_pattern.java
+++ charecter_pattern.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+public class charecter_pattern {
 
-public class invertedstar {
     public static void main(String args[])
     {
         Scanner scan=new Scanner(System.in);
-        int n=scan.nextInt();
-
+        int n;
+        n=scan.nextInt();
+        int ch=65;
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-(i-1);j++)
+            for(int j=1;j<=i;j++)
             {
-                System.out.print("* ");
+                System.out.print((char)ch+" ");
+                ch++;
+
             }
             System.out.println();
         }
-
     }
     
 }
--- halfpyramid_numbers.java
+++ halfpyramid_numbers.java
@@ -1,20 +1,19 @@
-import java.util.Scanner;
+import java.util.*;
+public class halfpyramid_numbers {
 
-public class invertedstar {
     public static void main(String args[])
     {
+
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
-
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=n-(i-1);j++)
+            for(int j=1;j<=i;j++)
             {
-                System.out.print("* ");
+                System.out.print(j+" ");
             }
             System.out.println();
         }
-
     }
     
 }
